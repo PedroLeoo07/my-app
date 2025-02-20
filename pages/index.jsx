f464ddc0595f62935a99113ed203fd854294635b
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-02-20 09:42:44 -0300

    index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home(){
+    return (
+        <div className={styles.container}>
+        <header className= {styles.header}>
+            <h1 className= {styles.title}>Amo a minha Mulher 💕</h1>
+        </header>
+        <main>
+            
+        </main>
+        </div>
+
+        
+    );
+} 
