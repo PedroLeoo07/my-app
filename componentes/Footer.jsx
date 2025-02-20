dad187420c2e5372ae551e61892198373c66f295
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-02-20 11:36:28 -0300

    add imgs

---
+++ componentes/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Footer.module.css";
+
+export default function Footer(){
+    return(
+        <footer className= {styles.footer}>
+        <h1 className={styles.title}>Amoo a minha mulher 🧡</h1>
+    </footer>
+    );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,39 @@
 import styles from "../styles/Home.module.css";
+import Header from "../componentes/Header";
+import Footer from "../componentes/Footer";
 
 export default function Home(){
     return (
         <div className={styles.container}>
-        <header className= {styles.header}>
-            <h1 className= {styles.title}>Amo a minha Mulher 💕</h1>
-        </header>
-        <main>
-            
-        </main>
+    <Header/>
+ 
+
+        <section className= {styles.section}>
+        <div className={styles.card}>
+            <img className={styles.imgs} src="/cr7.jpg" alt="" />
         </div>
+
+        <div className={styles.card}>
+        <img className={styles.imgs} src="/yuri.jpg" alt="" />
+        </div>
+
+        <div className={styles.card}>
+
+        <img className={styles.imgs} src="/ney.jpg" alt="" />
+        </div>
+
+        </section>
+        <div>   
+            
+        </div>
+        <aside className= {styles.aside}>
+        <div className={styles.card2}>
+        <h1>Teste do semantic</h1>
+        </div>
+        </aside>
+        <Footer/>
+        </div>
+        
 
         
     );
